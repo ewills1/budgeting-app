20c5c249af0d829017a1d41a72554d958058a672
Ed Wills <117778434+ewills1@users.noreply.github.com>
2026-03-29 12:28:04 +0100

    Add BudgetNotFoundException exception class

---
+++ budgeting-app-core/budget-service/src/main/java/com/budgeting/budget/exception/BudgetNotFoundException.java
@@ -0,0 +1,7 @@
+package com.budgeting.budget.exception;
+
+public class BudgetNotFoundException extends RuntimeException {
+    public BudgetNotFoundException(String message) {
+        super(message);
+    }
+}
