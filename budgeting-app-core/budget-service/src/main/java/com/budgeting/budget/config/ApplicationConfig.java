e19980dcc40a5788ba38a073d7f4d5817099e7ae
Ed Wills <edwardwills10@gmail.com>
2026-03-31 18:40:14 +0100

    Added api gateway

--- budgeting-app-core/budget-service/src/main/java/com/budgeting/budget/config/ApplicationConfig.java
+++ budgeting-app-core/budget-service/src/main/java/com/budgeting/budget/config/ApplicationConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @RequiredArgsConstructor
